578312a3904c4e7632876f674c2714e24d254a3c
relipse <relipse@gmail.com>
2013-02-01 15:24:42 -0800

    main javascript file

---
+++ SetFormElVal.js
@@ -0,0 +1,35 @@
+/**
+ * Set a form element's value|checked
+ * @param element elm the form element to set
+ * @param mixed value the value to set, true/false for checkboxes and radio boxes string for everything else
+ * @return boolean true on success
+ */
+function SetFormElementValue(elm, value){ 
+    if (typeof(elm.tagName) == 'undefined' && elm.length > 0 && elm[0].type == 'radio'){
+        //radio boxes
+        for (var i = 0; i < elm.length; ++i){
+                if (elm[i].value == value){
+                     elm[i].checked = true;
+                }else{ //if it does not match, make sure it is unchecked
+                     elm[i].checked = false;
+                }                
+        }
+        return true;
+    }else if (elm.type == 'checkbox' || elm.type == 'radio'){
+        if (value === false || value === ''){
+          elm.checked = false;   
+        }else{
+          elm.checked = true;
+          //if the user passes in true, just check the box
+          if (value !== true){
+             //actually change the post value!
+             elm.value = value;            
+          }
+        }
+        return true;
+    }else{
+      //not a radio box or a checkbox, just change the value
+      elm.value = value;
+      return true;
+    }
+}
